df9e7fe9fa19fac4f2e560648b7973b240bbbbc3
Matttran7 <matttran385701@gmail.com>
2023-06-20 14:52:45 -0700

    dynamic placeholder implemented
    placeholder now randomly picks from list in US_States and types it out in the input box as a placeholder

--- JS/script.js
+++ JS/script.js
@@ -16,7 +16,7 @@ input.onkeyup = (e)=>{
         search_contain.classList.add("active");
         showStates(arr);
         let full_arr = dropdown.querySelectorAll("li");
-        console.log(full_arr)
+        //console.log(full_arr)
         for(let k = 0; k<full_arr.length; k++){
             // make each li clickable
             full_arr[k].setAttribute("onclick", "select(this)");
@@ -43,3 +43,58 @@ function showStates(lst){
     }
     dropdown.innerHTML = lst_data;
 }
+
+// Placeholder text
+// lets store on stack to push and pop, when can't push anymore from word pop
+// when can't pop anymore bc stack is empty, find a new word and push
+let per_word = 0 
+let placeholder = ""
+const txt_list = []
+const speed = 100
+var slept = false;
+
+async function placeholder_fill(){
+    if (!txt_list.length && placeholder){ 
+        // slowly remove each character from placeholder
+        if(!slept){
+            await delay(2000)
+        }
+        if(placeholder.slice(-1) == " "){ // if it is a space character, remove another
+            placeholder = placeholder.substring(0,placeholder.length-1);
+        }
+        placeholder = placeholder.substring(0,placeholder.length-1);
+        document.getElementById("input_box").setAttribute("placeholder", placeholder);
+    }
+    else{
+        slept = false;
+        if (!txt_list.length){
+            // make a new list with each char of word
+            let temp_word = getRandomState();
+            for (let k = 0; k < temp_word.length;k++){
+                txt_list.push(temp_word[k]);
+            }
+        }
+        let letter =  txt_list.shift();
+        placeholder += letter;
+        if(letter == " "){ // if it's a space, add the next aswell
+            placeholder += txt_list.shift();
+        }
+        document.getElementById("input_box").setAttribute("placeholder", placeholder);
+    }
+    setTimeout(placeholder_fill,speed);
+}
+function getRandomState(){
+    let rand_index = Math.floor(Math.random() * US_States.length);
+    return US_States[rand_index]
+}
+/**
+ * Delay issue solved by Alvaro Trigo 
+ * site: https://alvarotrigo.com/blog/wait-1-second-javascript/
+ */
+function delay(milliseconds){
+    slept = true;
+    return new Promise(resolve => {
+        setTimeout(resolve, milliseconds);
+    });
+}
+placeholder_fill()
