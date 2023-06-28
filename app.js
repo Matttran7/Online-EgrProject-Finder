f76c015a6e4dde9ff5ed0605b305cdbc40439bb6
Matttran7 <matttran385701@gmail.com>
2023-06-27 19:29:50 -0700

    successfully made connection to mongodb

--- app.js
+++ app.js
@@ -1,24 +1,30 @@
 const express = require('express');
+const mongoose = require('mongoose');
 const app = express();
 const path = require('path');
 const router = express.Router();
 
+// Connect to mongodb
+var CONFIG = require('./config.json');
+const uri = 'mongodb+srv://'+CONFIG.mUser+':'+CONFIG.mPass+'@pmcluster.kpll3ey.mongodb.net/';
+
+async function connect(){
+    try {
+        await mongoose.connect(uri);
+        console.log("Successfully connected");
+    } catch (error){
+        console.log(error);
+    }
+}
+
+// express
 app.use(express.static(__dirname+"/view"));
 
 app.get('/',function(req,res) {
     res.sendFile(__dirname+'/'+'index.html');
   });
-/*app.get('/style.css', function(req, res) {
-    res.sendFile(__dirname + "/" + "style.css");
-});
-app.get('/script.js', function(req, res) {
-    res.sendFile(__dirname + "/JS/" + "script.js");
-});
-app.get('/US_States.js', function(req, res) {
-    res.sendFile(__dirname + "/JS/" + "US_States.js");
-});*/
 
 app.use('/', router);
 app.listen(process.env.port || 3000);
-
 console.log('Running at Port 3000');
+connect();
