286e4c9cb61871a9178d2e22d570af3597787811
Matttran7 <matttran385701@gmail.com>
2023-07-13 20:05:08 -0700

    cleaning

--- view/search/search_list.js
+++
@@ -1,4 +0,0 @@
-/**
- * List to store all possible searched data
- */
-var search_list = [];
--- view/search/searched.js
+++
@@ -1,8 +0,0 @@
-
-/**function DisplayData(){
-    console.log(search_list+ "-Search List-");
-    document.getElementById("temp").innerHTML+= search_list.toString();
-}
-
-DisplayData();
-*/
