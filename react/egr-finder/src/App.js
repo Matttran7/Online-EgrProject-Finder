65f34e08ea5604c3d62cd59aee4364971c4a2770
Matttran7 <matttran385701@gmail.com>
2023-07-27 15:42:12 -0700

    placeholder filling almost

--- react/egr-finder/src/App.js
+++ react/egr-finder/src/App.js
@@ -1,16 +1,13 @@
 import React, { useState, useEffect, useRef } from 'react';
 //import './style.css';
 import US_States from './US_States';
-import { handleInputChangeLogic, placeholderFillLogic } from './SearchLogic';
+import { handleInputChangeLogic} from './SearchLogic';
 
 function App() {
   const [placeholder, setPlaceholder] = useState('');
-  const [txtList, setTxtList] = useState([]);
+  const [setTxtList] = useState([]);
   const [inputValue, setInputValue] = useState(''); // input value
   const [filteredOptions, setFilteredOptions] = useState([]); // filtered options
-  const speed = 100;
-  const [slept, setSlept] = useState(false);
-  const searchContainRef = useRef(null);
   const dropdownRef = useRef(null);
 
   const handleInputChange = (e) => {
@@ -18,18 +15,65 @@ function App() {
     handleInputChangeLogic(e.target.value.trimStart(), setPlaceholder, setTxtList, setFilteredOptions);
   };
 
+  // ========================================================================= //
+  // Placeholder text
+  // lets store on stack to push and pop, when can't push anymore from word pop
+  // when can't pop anymore bc stack is empty, find a new word and push
+  const txt_list = [];
+  const speed = 100;
+  var slept = false;
+  let placeholderTemp = "";
+  async function placeholder_fill2(){
+    if (!txt_list.length && placeholderTemp){ 
+        // slowly remove each character from placeholder
+        if(!slept){ 
+            await delay(2000);
+        } 
+        if(placeholderTemp.slice(-1) === " "){ // if it is a space character, remove another
+            placeholderTemp = placeholderTemp.substring(0,placeholderTemp.length-1);
+        }
+        placeholderTemp = placeholderTemp.substring(0,placeholderTemp.length-1);
+        setPlaceholder(placeholderTemp);
+    }
+    else{
+        slept = false;
+        if (!txt_list.length){
+            // make a new list with each char of word
+            let temp_word = getRandomState();
+            for (let k = 0; k < temp_word.length;k++){
+                txt_list.push(temp_word[k]);
+            }
+        }
+        let letter =  txt_list.shift();
+        placeholderTemp += letter;
+        if(letter === " "){ // if it's a space, add the next aswell
+            placeholderTemp += txt_list.shift();
+        }
+        setPlaceholder(placeholderTemp);
+    }
+    setTimeout(placeholder_fill2,speed);
+  }
+  function delay(milliseconds){
+    slept = true;
+    return new Promise(resolve => {
+        setTimeout(resolve, milliseconds);
+    });
+  }
+  //=============================================================================//
+  
   useEffect(() => {
+    placeholder_fill2();
+    });
+
+  /*useEffect(() => {
     placeholderFillLogic(
       placeholder,
       setPlaceholder,
       txtList,
       setTxtList,
-      speed,
-      slept,
-      searchContainRef.current,
-      dropdownRef.current
+      searchContainRef.current
     );
-  }, [placeholder, setPlaceholder, txtList, setTxtList, speed, slept]);
+  }, [placeholder, setPlaceholder, txtList, setTxtList]);*/
 
   const handleSelectOption = (option) => {
     setInputValue(option);
@@ -67,6 +111,11 @@ function App() {
       </div>
     </div>
   );
+}
+
+function getRandomState() {
+  const randIndex = Math.floor(Math.random() * US_States.length);
+  return US_States[randIndex];
 }
 
 export default App;
--- react/egr-finder/src/SearchLogic.js
+++ react/egr-finder/src/SearchLogic.js
@@ -6,59 +6,15 @@ let slept = false;
 export function handleInputChangeLogic(inputValue, setPlaceholder, setTxtList, setFilteredOptions) {
   let i = inputValue.trimStart();
   let arr = [];
-
+  
   if (i) {
     arr = US_States.filter((data) => data.toLowerCase().startsWith(i.toLowerCase()));
     setFilteredOptions(arr);
     console.log("2")
   } else {
     setFilteredOptions([]);
+
   }
-}
-
-
-export async function placeholderFillLogic(
-  placeholder,
-  setPlaceholder,
-  txtList,
-  setTxtList,
-  speed,
-  slept,
-  search_contain,
-  dropdown
-) {
-  if (txtList && txtList.length) {
-    let letter = txtList.shift();
-    placeholder += letter;
-    if (letter === ' ') {
-      placeholder += txtList.shift();
-    }
-    setPlaceholder(placeholder);
-  } else if (placeholder) {
-    if (!slept) {
-      await delay(2000);
-    }
-    if (placeholder.slice(-1) === ' ') {
-      placeholder = placeholder.substring(0, placeholder.length - 1);
-    }
-    placeholder = placeholder.substring(0, placeholder.length - 1);
-    setPlaceholder(placeholder);
-  }
-
-  setTimeout(
-    () =>
-      placeholderFillLogic(
-        placeholder,
-        setPlaceholder,
-        txtList,
-        setTxtList,
-        speed,
-        slept,
-        search_contain,
-        dropdown
-      ),
-    speed
-  );
 }
 
 /**
