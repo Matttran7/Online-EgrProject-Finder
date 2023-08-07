9bf37f30ae87088330bee75386e5d5d8c62736a0
Matttran7 <matttran385701@gmail.com>
2023-08-06 22:01:22 -0700

    transition now working for /locations

--- react/egr-finder/src/Pages/locations.js
+++ react/egr-finder/src/Pages/locations.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { useLocation } from 'react-router-dom';
 import './locationStyle.css'
 
@@ -12,24 +12,42 @@ function Location(){
     if (location.state && location.state.loc) {
         locationName = location.state.loc;
     }
+    const [expandedStates, setExpandedStates] = useState(Array(locationData.length).fill(false));
 
+    const handleToggleExpansion = (index) => {
+      setExpandedStates(prevStates => {
+        const updatedStates = [...prevStates];
+        updatedStates[index] = !updatedStates[index];
+        return updatedStates;
+      });
+    };
+  
     return(
-    <div>
-        <div className="location">
-            <h1>{locationName}</h1>
+    <div className="page-container">
+        <div className="header-container">
+            <div className="location">
+                <h1>{locationName}</h1>
+            </div>
         </div>
-        <ul>
-        {locationData.map((item) => (
-          <li key={item._id}>
-            <strong className="project"> {item.project}</strong><br />
-            <strong className="group">{item.group}</strong><br />
-            <strong className="description">{item.description}</strong><br /><br />
-            <strong className="email">{item.email}</strong><br />
-          </li>
-        ))}
-      </ul>
+      <div className="main-container">
+          {locationData.map((item, index) => (
+              <div
+                  key={item._id}
+                  className={`expandContainer ${expandedStates[index] ? "expanded" : ""}`}
+                  onClick={() => handleToggleExpansion(index)}
+              >
+                  <div className="noDrop">
+                      <strong className="project">{item.project}</strong><br />
+                      <strong className="group">{item.group}</strong><br />
+                  </div>
+                  <div className={`drop ${expandedStates[index] ? "expanded" : ""}`}>
+                      <strong className="description">{item.description}</strong><br /><br />
+                      <strong className="email">{item.email}</strong><br />
+                  </div>
+              </div>
+          ))}
       </div>
+  </div>
     );
 }
-
 export default Location;
